ed0607f1a650c945be148519e9332bd10be8abc0
Nurnabi Khan <nurnabi7002@gmail.com>
2024-11-05 15:41:00 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Nav Bar Condition
+
+function openNav() {
+    document.getElementById("sidebar").style.width = "300px";
+    document.getElementById("sidebar").style.height = "100vh";
+}
+
+function closeNav() {
+    document.getElementById("sidebar").style.width = "0";
+    
+}
+   
+   
